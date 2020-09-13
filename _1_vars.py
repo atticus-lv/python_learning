ff5f5726c45c94957f56452c6f7ac72b7f9960a2
Atticus_id <1029910278@qq.com>
2020-09-13 17:02:25 +0800

    xxx

---
+++ _1_vars.py
@@ -0,0 +1,70 @@
+#-*- coding =utf-8 -*-
+
+
+# 注释方式
+
+'''
+写你的注释
+这里也是注释
+'''
+
+# 变量命名
+# a = 1
+#
+# aNumber = 1
+#
+# anumber = 1
+#
+# number_of_a = 1
+
+
+# 变量类型 整数
+
+# b = 2
+#
+# # 变量类型 字符串
+#
+# s = "这是一个字符串"
+# s1 = "xiaoming said:'this is a str'"
+
+# 变量类型 浮点
+
+# c = 1.0
+
+# 数学运算
+
+
+# 使用 print 输出
+
+# print(s,type(s))
+# print(b,type(b))
+# print(c,type(c))
+
+# 强制类型转换
+
+# str = "666"
+# str = 66.66
+#
+# zhuanhuan = int(str)
+# type_str = type(zhuanhuan)
+# print(zhuanhuan,type_str)
+
+# ints = 1
+# f = float(ints)
+# print(f)
+
+
+
+
+# 格式化输出（占位符）
+a = 1
+b = 10
+c = "字符串"
+d ="xxx"
+# e = "C:\\Users\\atticus\\Desktop\\save demo\\test scene\\tips"
+# print("a的值是%d"%a)
+# print("b的值是%d，c的值是%s"%(b,c))
+
+print(f"a的值是{a*b}，\nb的值是{b}，\tc的值是{c}")
+# print(e)
+# 关于字符串的一些简单处理
---
+++ _4_for.py
@@ -0,0 +1,31 @@
+#-*- coding =utf-8 -*-
+
+# for 循环
+a = 5
+s = 9
+
+
+for i in range(1,a+1):
+
+    if s<5:
+        break
+    else:
+        s = s - 1
+    print(s)
+
+
+
+# 遍历次数
+
+
+# 中断与继续
+
+
+
+# 遍历列表
+
+
+# 遍历字典
+
+
+# while 循环
